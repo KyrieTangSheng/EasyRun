20aa8d13725606a1a6d642523820cc80deda2001
Jay <w742794749@gmail.com>
2022-11-16 01:56:54 +0800

    test

--- frontend/src/profile edit pages/EditInsInfo.js
+++ frontend/src/profile edit pages/EditInsInfo.js
@@ -13,7 +13,8 @@ export default function EditInsInfo(props) {
     description: insData.description,
   });
 
-  const setShowAlert = props.setShowAlert;
+
+  const setShowAlert = props.setShowAlert
 
   const handleURL = (e) => {
     e.preventDefault();
